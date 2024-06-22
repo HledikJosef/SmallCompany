40f04f21047b01c353995b3455a78eb18efe8413
Josef Hledík <hledikjbc@gmail.com>
2024-06-22 22:30:06 +0200

    Revert "pokus o verzi s uživately"
    This reverts commit bfb943055bd790488657a7baca73c8d9d6012289.

--- SmallCompany.ClientBlazor/Program.cs
+++ SmallCompany.ClientBlazor/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Identity;
 using SmallCompany.ClientBlazor.Data;
 using SmallCompany.CompositionRoot;
 
@@ -15,45 +14,11 @@ namespace SmallCompany.ClientBlazor
                 // Add services to the container.
                 builder.Services.AddRazorPages();
                 builder.Services.AddServerSideBlazor();
-                builder.Services.AddHttpContextAccessor();
                 builder.Services.AddSingleton<WeatherForecastService>();
 
                 string? connectionString = builder.Configuration.GetConnectionString("SmallCompanyDb")
                         ?? throw new InvalidOperationException();
                 builder.Services.AddServices(connectionString);
-
-                builder.Services.Configure<IdentityOptions>(options =>
-                {
-                    // Password settings.
-                    options.Password.RequireDigit = true;
-                    options.Password.RequireLowercase = true;
-                    options.Password.RequireNonAlphanumeric = false;
-                    options.Password.RequireUppercase = true;
-                    options.Password.RequiredLength = 8;
-                    options.Password.RequiredUniqueChars = 0;
-
-                    // Lockout settings.
-                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-                    options.Lockout.MaxFailedAccessAttempts = 5;
-                    options.Lockout.AllowedForNewUsers = true;
-
-                    // User settings.
-                    options.User.AllowedUserNameCharacters =
-                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-                    options.User.RequireUniqueEmail = false;
-                });
-
-                builder.Services.ConfigureApplicationCookie(options =>
-                {
-                    // Cookie settings
-                    options.Cookie.HttpOnly = true;
-                    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-
-                    options.LoginPath = "/Identity/Account/Login";
-                    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-                    options.SlidingExpiration = true;
-                });
-
 
                 var app = builder.Build();
 
@@ -71,10 +36,6 @@ namespace SmallCompany.ClientBlazor
 
                 app.UseRouting();
 
-                app.UseAuthentication();
-                app.UseAuthorization();
-
-                app.MapControllers();
                 app.MapBlazorHub();
                 app.MapFallbackToPage("/_Host");
 
