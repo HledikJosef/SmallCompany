7f5d82fd530c379d89746ac56d2632dc7c7e772a
Josef Hledík <hledikjbc@gmail.com>
2024-11-16 17:16:06 +0100

    CheckExistingItemAsync předělán do EF. Vyřešit problém s Collate v databázi

--- SmallCompany.DataLayer/Implementation/ItemDao.cs
+++ SmallCompany.DataLayer/Implementation/ItemDao.cs
@@ -1,102 +1,73 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SmallCompany.Models;
 using SmallCompany.Models.Data;
-using System.Data;
-using System.Data.SqlClient;
 
 namespace SmallCompany.DataLayer.Implementation
 {
-	public class ItemDao : IItemDao
-	{
-		private readonly ApplicationDbContext context;
-		private readonly string connectionString;
-		public ItemDao(ApplicationDbContext context)
-		{
-			this.context = context;
-			connectionString = context.Database.GetDbConnection().ConnectionString;
-		}
+    public class ItemDao : IItemDao
+    {
+        private readonly ApplicationDbContext context;
+        private readonly string connectionString;
+        public ItemDao(ApplicationDbContext context)
+        {
+            this.context = context;
+            connectionString = context.Database.GetDbConnection().ConnectionString;
+        }
 
-		public async Task<List<Item>> GetItemsFromSqlAsync()
-		{
-			List<Item> itemsFromSql = new List<Item>();
-			itemsFromSql = await context.Items.ToListAsync();
+        public async Task<List<Item>> GetItemsFromSqlAsync()
+        {
+            List<Item> itemsFromSql = new List<Item>();
+            itemsFromSql = await context.Items.ToListAsync();
 
-			return itemsFromSql;
-		}
+            return itemsFromSql;
+        }
 
-		public async Task AddItemWithPropertiesToDbAsync(Item itemWithProperties)
-		{
-			List<Item> existingItems = await CheckExistingItemAsync(itemWithProperties);
+        public async Task AddItemWithPropertiesToDbAsync(Item itemWithProperties)
+        {
+            List<Item> existingItems = await CheckExistingItemAsync(itemWithProperties);
 
-			if (existingItems.Count() == 0)
-			{
-				await context.Items.AddAsync(itemWithProperties);
-				await context.SaveChangesAsync();
-			}
-		}
+            if (existingItems.Count() == 0)
+            {
+                await context.Items.AddAsync(itemWithProperties);
+                await context.SaveChangesAsync();
+            }
+        }
 
-		private async Task<List<Item>> CheckExistingItemAsync(Item item)
-		{
-			List<Item> existingItems = new List<Item>();
+        private async Task<List<Item>> CheckExistingItemAsync(Item item)
+        {
+            List<Item> existingItems = new List<Item>();
 
-			string sqlQuery = @"SELECT i.Id FROM Items i INNER JOIN ItemsPropertyValue ipv ON i.Id = ipv.ItemId WHERE i.Name = @Name";
+            string sqlQuery = $"SELECT i.* FROM Items i WHERE i.Name = '{item.Name}' AND i.Id IN ( SELECT ipv1.ItemId FROM ItemsPropertyValue ipv1 WHERE ";
 
-			int index = 1;
+            int index = 0;
 
-			foreach (var ipv in item.ItemPropertyValues)
-			{
-				sqlQuery += ($" AND EXISTS( SELECT 1 FROM ItemsPropertyValue ipv{index} WHERE ipv{index}.ItemId = i.Id AND ipv{index}.PropertyId = @PropertyId{index} AND ipv{index}.Value = @Value{index} )");
+            string whereClause = "";
 
-				index++;
-			}
+            foreach (var ipv in item.ItemPropertyValues)
+            {
+                if (!string.IsNullOrEmpty(whereClause))
+                {
+                    whereClause += "OR ";
+                }
 
-			index = 1;
+                whereClause += $"(ipv1.PropertyId = {ipv.PropertyId} AND ipv1.Value = '{ipv.Value}' ) ";
 
-			using (SqlConnection connection = new SqlConnection(connectionString))
-			{
-				using (SqlCommand command = new SqlCommand(sqlQuery, connection))
-				{
+                index++;
 
-					command.Parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar));
-					command.Parameters["@Name"].Value = item.Name;
+            }
 
-					foreach (var ipv in item.ItemPropertyValues)
-					{
-						command.Parameters.Add(new SqlParameter($"@PropertyId{index}", SqlDbType.Int));
-						command.Parameters[$"@PropertyId{index}"].Value = ipv.PropertyId;
+            sqlQuery += whereClause;
 
-						command.Parameters.Add(new SqlParameter($"@Value{index}", SqlDbType.NVarChar));
-						command.Parameters[$"@Value{index}"].Value = ipv.Value;
-
-						index++;
-					}
-					await connection.OpenAsync();
-
-					using (var reader = await command.ExecuteReaderAsync())
-					{
-						while (reader.Read())
-						{
-							Item existingItem = new Item
-							{
-								Id = reader.GetInt32(reader.GetOrdinal("Id")),
-							};
-							existingItems.Add(existingItem);
-
-						}
-
-					}
+            sqlQuery += $"GROUP BY ipv1.ItemId HAVING COUNT(DISTINCT ipv1.PropertyId) = {index} )";
 
 
-				}
+            existingItems = context.Items.FromSqlRaw(sqlQuery).ToList();
 
-			}
-
-
-			return existingItems;
-		}
+            return existingItems;
+        }
 
 
 
-	}
+    }
 }
 
